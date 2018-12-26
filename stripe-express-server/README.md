29e1a4f1efcf4adf1e0ffb12787adc45d9f2beda
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-12-26 09:41:58 +0000

    initial commit

---
+++ stripe-express-server/README.md
@@ -0,0 +1,12 @@
+# Stripe Express Server
+
+## How this was created
+references:
+```
+https://stripe.com/docs/recipes/elements-react
+https://stripe.com/docs/stripe-js
+```
+- In your terminal, install
+    ```
+    npm install express body-parser stripe
+    ```
---
+++ stripe-express-server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const app = require("express")();
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = require("stripe")(process.env.STRIPE_TEST_SECRET);
+
+app.use(require("body-parser").text());
+
+
+app.post("/charge", async (req, res) => {
+  try {
+    let {status} = await stripe.charges.create({
+      amount: 2000,
+      currency: "usd",
+      description: "An example charge",
+      source: req.body
+    });
+
+    res.json({status});
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+
+app.listen(9000, () => console.log("Listening on port 9000"));
+
