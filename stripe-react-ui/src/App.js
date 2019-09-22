bb446b2ab4460a28c1a90b895a9f4b7b40614f1b
Noby Fujioka <n.fujioka@abacusmaths.info>
2019-09-22 01:11:42 +0100

    adds update

--- stripe-react-ui/src/App.js
+++ stripe-react-ui/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import CheckoutForm from './CheckoutForm';
+import CheckoutFormUpdate from './CheckoutFormUpdate';
 import './App.css';
 
 
@@ -8,9 +9,13 @@ const App = () => {
   return (
     <StripeProvider apiKey="pk_test_2aTYM95z89mk47yuj78Es5sE">
       <div className="example">
-        <h1>React Stripe Elements Example</h1>
+        <h2>React Stripe Elements Example 1 - Old Way</h2>
         <Elements>
           <CheckoutForm />
+        </Elements>
+        <h2>React Stripe Elements Example 2 - New Way with PaymentIntent (SCA ready)</h2>
+        <Elements>
+          <CheckoutFormUpdate />
         </Elements>
       </div>
     </StripeProvider>
---
+++ stripe-react-ui/src/CheckoutFormUpdate.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState }from 'react';
+import { CardElement, injectStripe } from 'react-stripe-elements';
+import './CheckoutForm.css';
+
+const CheckoutForm = (props) => {
+  useEffect(() => {
+    fetch("/start-payment")
+      .then(res => res.json())
+      .then(result => {
+        setClientSecret(result.client_secret);
+      })
+      .catch(err => {
+        console.log('err!!>>>', err);
+      })
+  }, []);
+  const [ cardElement, setCardElement ] = useState(null);
+  const [ clientSecret, setClientSecret ] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const [complete, setComplete] = useState(false);
+  const [ errorMessage, setErrorMessage ] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setDisabled(true);
+    const {paymentIntent, error} = await props.stripe.handleCardPayment(
+      clientSecret, cardElement, {
+        payment_method_data: {
+          billing_details: {name: "Tester Toby"}
+        },
+        receipt_email: "test@test.com"
+      }
+    );
+    if (error) {
+      // Display error.message in your UI.
+      console.log('error!!!!!!!', error);
+      setErrorMessage('Payment failed');
+    } else {
+      // The payment has succeeded. Display a success message.
+      console.log("Purchase Complete!");
+      console.log('paymentIntent!!!!>>', paymentIntent);
+      setComplete(true);
+    }
+    setDisabled(false);
+  };
+
+  const handleReady = (element) => {
+    setCardElement(element);
+  };
+
+  if (complete) return <p>Purchase Complete</p>;
+
+  return (
+    <div className="checkout">
+      <p>Would you like to complete the purchase?</p>
+      <CardElement
+        onReady={handleReady}
+      />
+      {
+        errorMessage && (
+          <p>{errorMessage}</p>
+        )
+      }
+      {
+        disabled
+          ? <p>Processing ... </p>
+          : <button onClick={onSubmit} disabled={disabled}>Send</button>
+      }
+    </div>
+  );
+};
+
+
+export default injectStripe(CheckoutForm);
