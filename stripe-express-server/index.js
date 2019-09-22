b429d8b537f1b76affb6d9fa7c6d6a129e5991f9
Noby Fujioka <n.fujioka@abacusmaths.info>
2019-09-22 01:10:36 +0100

    - adds example with paymentIntent

--- stripe-express-server/index.js
+++ stripe-express-server/index.js
@@ -3,24 +3,67 @@ const app = require("express")();
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 var stripe = require("stripe")(process.env.STRIPE_TEST_SECRET);
+const bodyParser = require('body-parser'); //Help parse incoming HTTP requests
+app.use(bodyParser.text());
 
-app.use(require("body-parser").text());
-
+app.get('/hello', (req, res) => {
+  res.json({ message: "Hello, world!" });
+});
 
 app.post("/charge", async (req, res) => {
-  console.log('req.body>>>', req.body);
+  // Traditional Stripe - No longer use this endpoint.
   try {
     let {status} = await stripe.charges.create({
-      amount: 2000,
-      currency: "usd",
+      amount: 4000,
+      currency: "gbp",
       description: "An example charge",
-      source: req.body   // req.body contains token sent from the FE
+      source: req.body  // req.body will be a token string sent from the FE
     });
 
     res.json({status});
   } catch (err) {
     res.status(500).end();
   }
+});
+
+app.get("/start-payment", async (req, res) => {
+  // Call this endpoint to set paymentIntents first.
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 200000,
+    currency: 'gbp',
+    payment_method_types: ['card'],
+    description: "testing paymentIntent"
+  });
+  // Return client_secret for the paymentIntent created above to the Front end.
+  res.json({client_secret: paymentIntent.client_secret});
+});
+
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
+  // Set stripe dashboard so that stripe will trigger this endpoint when paymentIntent status changes.
+  console.log('contacted by webhook!!!');
+
+  let event = null;
+  try {
+    event = JSON.parse(request.body) || {};
+  }
+  catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  let intent = null;
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      intent = event.data.object;
+      console.log("Succeeded:", intent.id);
+      break;
+    case 'payment_intent.payment_failed':
+      intent = event.data.object;
+      const message = intent.last_payment_error && intent.last_payment_error.message;
+      console.log('Failed:', intent.id, message);
+      break;
+  }
+
+  response.sendStatus(200);
 });
 
 app.listen(9000, () => console.log("Listening on port 9000"));
--- stripe-react-ui/src/CheckoutForm.js
+++ stripe-react-ui/src/CheckoutForm.js
@@ -5,31 +5,40 @@ import './CheckoutForm.css';
 const CheckoutForm = (props) => {
   const [disabled, setDisabled] = useState(false);
   const [complete, setComplete] = useState(false);
+  const [ errorMessage, setErrorMessage ] = useState('');
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    const {token} = await props.stripe.createToken({name: "Example guy"});
+    setDisabled(true);
+    const {token} = await props.stripe.createToken({name: "Mike Cardman"});
     if (token) {
-      setDisabled(true);
       const response = await fetch("/charge", {
         method: "POST",
         headers: {"Content-Type": "text/plain"},
         body: token.id
       });
+      console.log('response>>>>', response);
       if (response.ok) {
         console.log("Purchase Complete!");
         setComplete(true);
-        setDisabled(true);
+      } else {
+        setErrorMessage('Payment failed');
       }
     }
+    setDisabled(false);
   };
 
-  if (complete) return <h1>Purchase Complete</h1>;
+  if (complete) return <p>Purchase Complete</p>;
 
   return (
     <div className="checkout">
       <p>Would you like to complete the purchase?</p>
       <CardElement />
+      {
+        errorMessage && (
+          <p>{errorMessage}</p>
+        )
+      }
       {
         disabled
           ? <p>Processing ... </p>
